4860248a39871c548447b94fb37b35e2d23d14e3
IIT\kriahidehkordi <kourosh.riahidehkordi@iit.it>
2024-04-23 13:10:26 +0200

    initial starter files

---
+++ Design Patterns/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Your additional includes here
+
+int main() {
+    // Your code here
+
+    std::cout << "Hello, world!" << std::endl;
+
+    // Additional code
+
+    return 0;
+}
