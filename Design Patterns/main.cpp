6927f83dbe351b2e68663369c58b5547c0a9a4d0
IIT\kriahidehkordi <kourosh.riahidehkordi@iit.it>
2024-05-08 17:57:08 +0200

    Factory Method Pattern

--- Design Patterns/main.cpp
+++ Design Patterns/main.cpp
@@ -1,89 +1,61 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
+#include <memory>
 
-// Character struct representing a game character
-struct Character {
-    int x;
-    int y;
-    std::string name;
+// Abstract Product
+class Product {
+public:
+    virtual void operation() = 0;
+    virtual ~Product() {}
+};
 
-    // Constructor
-    Character(std::string _name) : name(_name), x(0), y(0) {}
-
-    // Method to move the character
-    void move(int _x, int _y) {
-        x = _x;
-        y = _y;
+// Concrete Products
+class ConcreteProductA : public Product {
+public:
+    void operation() override {
+        std::cout << "ConcreteProductA operation\n";
     }
 };
 
-// Command interface
-class Command {
+class ConcreteProductB : public Product {
 public:
-    virtual ~Command() {}
-    virtual void execute(Character& c) = 0;
-    virtual void undo(Character& c) = 0;
+    void operation() override {
+        std::cout << "ConcreteProductB operation\n";
+    }
 };
 
-// Move command class
-class Move : public Command {
+// Abstract Creator
+class Creator {
 public:
-    void execute(Character& c) override {
-        int randomx = rand() % 4;
-        int randomy = rand() % 4;
-        c.move(randomx, randomy);
-        x = randomx;
-        y = randomy;
-        std::cout << c.name << " moved " << x << "," << y << std::endl;
+    virtual std::unique_ptr<Product> factoryMethod() = 0;
+    void someOperation() {
+        std::unique_ptr<Product> product = factoryMethod();
+        product->operation();
     }
+    virtual ~Creator() {}
+};
 
-    void undo(Character& c) override {
-        c.move(-x, -y);
-        std::cout << c.name << " undo " << -x << "," << -y << std::endl;
+// Concrete Creators
+class ConcreteCreatorA : public Creator {
+public:
+    std::unique_ptr<Product> factoryMethod() override {
+        return std::make_unique<ConcreteProductA>();
     }
+};
 
-private:
-    int x;
-    int y;
+class ConcreteCreatorB : public Creator {
+public:
+    std::unique_ptr<Product> factoryMethod() override {
+        return std::make_unique<ConcreteProductB>();
+    }
 };
 
 int main() {
-    // Seed the random number generator
-    srand(time(NULL));
+    // Client code
+    std::unique_ptr<Creator> creatorA = std::make_unique<ConcreteCreatorA>();
+    creatorA->someOperation();
 
-    // Vector to hold command objects
-    std::vector<Command*> command_queue;
-
-    // Create a character
-    Character character("char");
-
-    // Create move commands and add them to the command queue
-    Move* move1 = new Move;
-    command_queue.push_back(move1);
-    Move* move2 = new Move;
-    command_queue.push_back(move2);
-    Move* move3 = new Move;
-    command_queue.push_back(move3);
-    Move* move4 = new Move;
-    command_queue.push_back(move4);
-
-    // Execute move commands for the character
-    for (int i = 0; i < command_queue.size(); i++) {
-        command_queue[i]->execute(character);
-    }
-
-    // Undo move commands for the character
-    for (int i = command_queue.size() - 1; i >= 0; i--) {
-        command_queue[i]->undo(character);
-    }
-
-    // Free memory allocated for command objects
-    for (Command* cmd : command_queue) {
-        delete cmd;
-    }
+    std::unique_ptr<Creator> creatorB = std::make_unique<ConcreteCreatorB>();
+    creatorB->someOperation();
 
     return 0;
 }
